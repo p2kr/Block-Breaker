7159d9d2d015c2ea5b180d9cc79225b1174a4eb8
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-09 19:50:14 +0530

    Lecture 63 (Udemy unity2d)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config parameters
+    [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
+    [SerializeField] private AudioClip[] ballSounds;
+
+    // state
+    private Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+    private Rigidbody2D rb;
+
+    // Cached component references
+    AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.velocity = new Vector2(xPush, yPush);
+            rb.simulated = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x,
+                    paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //  Configuration parameters
+    [SerializeField] private float minX = 1;
+    [SerializeField] private float maxX = 15f;
+    [SerializeField] private float screenWidthInUnits = 16;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        mousePosInUnits = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
