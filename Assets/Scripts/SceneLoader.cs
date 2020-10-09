ca9c7c773dafc4f312060aba35a9525cad4f3aaf
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-09 23:49:09 +0530

    Reset Game Session

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,8 +10,9 @@ public class SceneLoader : MonoBehaviour
     }
 
     public void LoadStartScene()
-    {
+    {        
         SceneManager.LoadScene(0);
+        FindObjectOfType<GameStatus>().ResetGame();
     }
 
     public void QuitGame()
