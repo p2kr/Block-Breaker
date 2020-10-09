c06a8ff6519dd583029270b5a8e9fd20d3cf719e
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-09 21:08:00 +0530

    Add Player Score

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,11 +7,13 @@ public class Block : MonoBehaviour
 
     // cached reference
     Level level;
+    GameStatus gameStatus;
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
+        gameStatus = FindObjectOfType<GameStatus>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -24,5 +26,6 @@ public class Block : MonoBehaviour
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlockDestroyed();
+        gameStatus.AddToScore();
     }
 }
