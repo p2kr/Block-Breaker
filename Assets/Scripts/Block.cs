d98a400eecab88ecbf8bea7415844614114e202c
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-09 20:31:25 +0530

    Lecture 65

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,8 +2,21 @@
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
