0a067ab3c829128727a17765ae8a1bc9810b52cf
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-09 20:43:09 +0530

    Lecture 66

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,9 +7,25 @@ public class Level : MonoBehaviour
 
     [SerializeField] int breakableBlocks; //Serialize for debugging purposes
 
+    // cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
     }
 
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
 }
